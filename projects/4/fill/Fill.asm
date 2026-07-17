04db68ca101c3e769bb6038ad0ce4dce2a22d858
vivek singh <pokhariyavivek01@gmail.com>
2026-07-17 13:22:20 +0530

    completed the fill.asm and mult.asm 'Assembly Code'

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -9,3 +9,66 @@
 // the screen should be cleared.
 
 //// Replace this comment with your code.
+@8192
+D=A
+@n
+M=D         // setting the value of 'n' == 8192
+
+(LOOP)
+@KBD
+D=M
+@SETSCREEN
+D;JGT
+@CLRSCREEN
+D;JEQ
+@LOOP
+0;JMP
+
+
+(SETSCREEN)
+@i
+M=0
+(SETCONTINUE)
+
+@i
+D=M
+@n
+D=D-M
+@LOOP
+D; JEQ      //if i==n goback to main loop
+
+@SCREEN
+D=A         // D = 16384 (Base address)
+@i
+A=D+M       // A = 16384 + i
+M=-1        // Fill 16 pixels black
+
+@i
+M=M+1       //increment 'i'
+@SETCONTINUE
+0; JMP
+
+
+(CLRSCREEN)
+@i
+M=0
+(CLRCONTINUE)
+
+@i
+D=M
+@n
+D=D-M
+@LOOP
+D; JEQ      //if i==n goback to main loop
+
+@SCREEN
+D=A         // D = 16384 (Base address constant)
+@i          // Select the index variable
+A=D+M       // A = 16384 + value of i
+M=0         // Set all 16 pixels to white
+
+@i
+M=M+1       //increment 'i'
+@CLRCONTINUE
+0;JMP
+
--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -8,3 +8,51 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+
+
+@R2
+M=0 //Store the first value in 'R2' register
+
+@R1
+D=M
+@LOOP
+D; JGE  //if R1 >= 0 then go to loop without changing signs 
+
+
+@R0
+D=M
+M=-D   // negate the value of R0 i.e. R0 = -1*R0
+
+@R1
+D=M
+M=-D  // similary negate R1
+
+
+// getting the mulitplication ans
+@R0
+D=M
+@R2
+M=D
+@i
+M=1 // Initializing the 'i' with 1
+
+(LOOP)
+@R1
+D=M
+@END
+D;JEQ  // if R1 == 0 no need to continue exit
+
+@R0
+D=M
+@R2
+M=M+D  // R2 = R2 + R0
+
+@R1
+M=M-1  // reduce the count of R1 by 1
+
+@LOOP
+0,JMP  // continue
+
+(END)
+@END
+0;JMP
